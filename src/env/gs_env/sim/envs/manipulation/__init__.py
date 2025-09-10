493693f5dcec2dae9e40d2de99e5881d525620c2
LeonLiu4 <leonjliu2016@gmail.com>
2025-09-10 14:23:53 -0700

    bowl in microwave environment

--- src/env/gs_env/sim/envs/manipulation/__init__.py
+++ src/env/gs_env/sim/envs/manipulation/__init__.py
@@ -1,5 +1,7 @@
 from .goal_reaching_env import GoalReachingEnv
+from .put_bowl_inside_microwave_env import PutBowlInsideMicrowaveEnv
 
 __all__ = [
     "GoalReachingEnv",
+    "PutBowlInsideMicrowaveEnv",
 ]
